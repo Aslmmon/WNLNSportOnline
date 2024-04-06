c01c44be15ecae37880495b1c7f0bcaba8d736a0
Aslm <aslmmon993@gmail.com>
2024-04-06 12:45:13 -0300

    added dialog in acheivments screen

--- app/src/main/java/com/winalane/sport/online/ui/acheivements/AchievementsScreen.kt
+++ app/src/main/java/com/winalane/sport/online/ui/acheivements/AchievementsScreen.kt
@@ -2,34 +2,27 @@ package com.winalane.sport.online.ui.acheivements
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.winalane.sport.online.R
-import com.winalane.sport.online.ui.workout.Sport
+import com.winalane.sport.online.ui.components.MinimalDialog
 
 @Composable
 fun AchievmentsScreen(modifier: Modifier) {
@@ -47,6 +40,17 @@ fun AchievmentsScreen(modifier: Modifier) {
 
         )
 
+    val openAlertDialog = remember { mutableStateOf(true) }
+    when {
+        openAlertDialog.value -> {
+            MinimalDialog(
+                modifier = modifier,
+                newScoreImage = R.drawable.lvl_1_achieved,
+                onDismissRequest = { openAlertDialog.value = false },
+            )
+        }
+    }
+
 
     Column(
 
@@ -58,7 +62,9 @@ fun AchievmentsScreen(modifier: Modifier) {
             painter = painterResource(id = R.drawable.level_track),
             contentDescription = "",
             contentScale = ContentScale.Crop,
-            modifier = modifier.align(Alignment.CenterHorizontally).size(80.dp)
+            modifier = modifier
+                .align(Alignment.CenterHorizontally)
+                .size(80.dp)
         )
         Spacer(modifier = modifier.height(10.dp))
         LazyVerticalGrid(
--- app/src/main/java/com/winalane/sport/online/ui/components/Components.kt
+++ app/src/main/java/com/winalane/sport/online/ui/components/Components.kt
@@ -1,18 +1,37 @@
 package com.winalane.sport.online.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.winalane.sport.online.R
 import com.winalane.sport.online.ui.workout.Sport
 
 
@@ -28,9 +47,10 @@ fun AppButton(modifier: Modifier, text: String, onClick: () -> Unit) {
     ) {
         Text(
             text = text,
-            modifier = modifier.padding(10.dp),
+            modifier = modifier.padding(horizontal = 15.dp, vertical = 5.dp),
             color = Color.White,
-            style = MaterialTheme.typography.labelMedium
+            style = MaterialTheme.typography.labelMedium,
+            textAlign = TextAlign.Center
         )
     }
 }
@@ -67,5 +87,67 @@ fun ItemsView(
             color = if (sport.isSelected) Color.White else MaterialTheme.colorScheme.secondary,
             style = MaterialTheme.typography.labelMedium
         )
+    }
+}
+
+@Composable
+fun MinimalDialog(modifier: Modifier, newScoreImage: Int, onDismissRequest: () -> Unit) {
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Card(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 40.dp)
+                .height(250.dp),
+            shape = RoundedCornerShape(8.dp),
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround,
+                modifier = modifier.padding(vertical = 10.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.new_level),
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 10.dp)
+                        .wrapContentSize(Alignment.Center),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.labelMedium
+                )
+
+
+                Box(modifier = modifier, contentAlignment = Alignment.TopCenter) {
+                    Divider(
+                        modifier = modifier.height(50.dp),
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                    Column (horizontalAlignment = Alignment.CenterHorizontally){
+                        Image(
+                            painter = painterResource(id = newScoreImage),
+                            contentDescription = "",
+                            contentScale = ContentScale.Crop,
+                            modifier = modifier
+                                .padding(top = 15.dp)
+                                .size(100.dp, 120.dp)
+
+                        )
+                        Text(
+                            text = "confidence",
+                            modifier = modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 10.dp)
+                                .wrapContentSize(Alignment.Center),
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.labelMedium,
+                            color = MaterialTheme.colorScheme.secondary
+                        )
+                    }
+                }
+
+                AppButton(modifier = modifier, text = "Thanks") {
+                    onDismissRequest.invoke()
+                }
+            }
+        }
     }
 }
