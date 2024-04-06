359555c62fc6fe73036cbda75fad1f5902362c40
Aslm <aslmmon993@gmail.com>
2024-04-06 12:16:24 -0300

    added ui for acheivements screen

--- app/src/main/java/com/winalane/sport/online/ui/acheivements/AchievementsScreen.kt
+++ app/src/main/java/com/winalane/sport/online/ui/acheivements/AchievementsScreen.kt
@@ -1,10 +1,97 @@
 package com.winalane.sport.online.ui.acheivements
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.winalane.sport.online.R
+import com.winalane.sport.online.ui.workout.Sport
 
 @Composable
-fun AchievmentsScreen(modifier: Modifier){
-    Text(text = "Achievements")
+fun AchievmentsScreen(modifier: Modifier) {
+    var listSports = mutableListOf<AchivementsData>(
+        AchivementsData(R.drawable.lvl_1_unachieved, R.drawable.lvl_1_achieved),
+        AchivementsData(R.drawable.lvl_2_unachieved, R.drawable.lvl_1_achieved),
+        AchivementsData(R.drawable.lvl_3_unachieved, R.drawable.lvl_1_achieved),
+        AchivementsData(R.drawable.lvl_4_unachieved, R.drawable.lvl_1_achieved),
+        AchivementsData(R.drawable.lvl_5_unachieved, R.drawable.lvl_1_achieved),
+        AchivementsData(R.drawable.lvl_6_unachieved, R.drawable.lvl_1_achieved),
+        AchivementsData(R.drawable.lvl_7_unachieved, R.drawable.lvl_1_achieved),
+        AchivementsData(R.drawable.lvl_8_unachieved, R.drawable.lvl_1_achieved),
+        AchivementsData(R.drawable.lvl_9_unachieved, R.drawable.lvl_1_achieved),
+        AchivementsData(R.drawable.lvl_10_unachieved, R.drawable.lvl_1_achieved),
+
+        )
+
+
+    Column(
+
+        modifier = modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.tertiary),
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.level_track),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = modifier.align(Alignment.CenterHorizontally).size(80.dp)
+        )
+        Spacer(modifier = modifier.height(10.dp))
+        LazyVerticalGrid(
+            modifier = modifier
+                .padding(horizontal = 10.dp)
+                .height(480.dp),
+            columns = GridCells.Fixed(3),
+            horizontalArrangement = Arrangement.spacedBy(5.dp, Alignment.CenterHorizontally),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+
+            ) {
+            itemsIndexed(listSports) { index, photo ->
+                Image(
+                    painter = painterResource(id = photo.unachievedIcon),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                )
+
+            }
+        }
+//        Image(
+//            modifier = modifier
+//                .size(100.dp)
+//                .align(Alignment.CenterHorizontally),
+//            painter = painterResource(id = listSports[listSports.lastIndex].unachievedIcon),
+//            contentDescription = "",
+//            contentScale = ContentScale.Crop,
+//        )
+    }
+
 }
+
+data class AchivementsData(
+    var unachievedIcon: Int,
+    var achievedIcon: Int
+)
--- app/src/main/java/com/winalane/sport/online/ui/progress/ProgressScreen.kt
+++ app/src/main/java/com/winalane/sport/online/ui/progress/ProgressScreen.kt
@@ -10,16 +10,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.winalane.sport.online.R
-import com.winalane.sport.online.ui.components.AppButton
 import com.winalane.sport.online.ui.components.ContainerBox
 import com.winalane.sport.online.ui.components.ItemsView
 import com.winalane.sport.online.ui.workout.Sport
